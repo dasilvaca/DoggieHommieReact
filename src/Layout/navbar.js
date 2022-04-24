6cf18299367d6934640470520e997b5c83c3db4c
dasilvaca <dasilvaca@unal.edu.co>
2022-04-23 21:19:29 -0500

    added the actual navbar

--- src/Layout/navbar.js
+++ src/Layout/navbar.js
@@ -1,65 +1,80 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/img/Doggie Hommie.png';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <a className="navbar-brand me-2" href="https://mdbgo.com/">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-            height="16"
-            alt="MDB Logo"
-            loading="lazy"
-            style={{ marginTop: '-1px' }}
-          />
-        </a>
+    // <nav classNameName="navbar navbar-expand-lg navbar-light bg-light">
+    //   <div classNameName="container">
+    //     <a classNameName="navbar-brand me-2" href="https://mdbgo.com/">
+    //       <img
+    //         src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+    //         height="16"
+    //         alt="MDB Logo"
+    //         loading="lazy"
+    //         style={{ marginTop: '-1px' }}
+    //       />
+    //     </a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-mdb-toggle="collapse"
-          data-mdb-target="#navbarButtonsExample"
-          aria-controls="navbarButtonsExample"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
+    //     <button
+    //       classNameName="navbar-toggler"
+    //       type="button"
+    //       data-mdb-toggle="collapse"
+    //       data-mdb-target="#navbarButtonsExample"
+    //       aria-controls="navbarButtonsExample"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <i classNameName="fas fa-bars"></i>
+    //     </button>
 
-        <div className="collapse navbar-collapse" id="navbarButtonsExample">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Dashboard
-              </a>
-            </li>
-          </ul>
+    //     <div classNameName="collapse navbar-collapse" id="navbarButtonsExample">
+    //       <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
+    //         <li classNameName="nav-item">
+    //           <a classNameName="nav-link" href="#">
+    //             Dashboard
+    //           </a>
+    //         </li>
+    //       </ul>
 
-          <div>
+    //       <div>
 
 
-          <div className="d-flex align-items-center">
-            <Link to="/login">
-              <button type="button" className="btn btn-link px-3 me-2">
-                Login
-              </button>
-            </Link>
-            <button type="button" className="btn btn-primary me-3">
-              Sign up for free
-            </button>
-            <a
-              className="btn btn-dark px-3"
-              href="https://github.com/mdbootstrap/mdb-ui-kit"
-              role="button"
-            >
-              <i className="fab fa-github" />
-            </a>
-          </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    //       <div classNameName="d-flex align-items-center">
+    //         <Link to="/login">
+    //           <button type="button" classNameName="btn btn-link px-3 me-2">
+    //             Login
+    //           </button>
+    //         </Link>
+    //         <button type="button" classNameName="btn btn-primary me-3">
+    //           Sign up for free
+    //         </button>
+    //         <a
+    //           classNameName="btn btn-dark px-3"
+    //           href="https://github.com/mdbootstrap/mdb-ui-kit"
+    //           role="button"
+    //         >
+    //           <i classNameName="fab fa-github" />
+    //         </a>
+    //       </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
+    <div className="navbar">
+    <div id="logo">
+      <Link to="/home">
+            <img src= {logo} style={{padding: "5px", alt:"logo"}}/>
+      </Link>
+    </div>
+    <div id="nav-links">
+        {/* <!-- <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a> --> */}
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </div>
+</div>
   );
 };
 
