3ee2a075d2fe93845a7ab2486cbf3f7f29f5c595
Juan Pablo Garson Parra <juromerop@unal.edu.co>
2022-05-02 17:55:07 -0500

    Cambios en el post

--- src/components/post.js
+++ src/components/post.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { FaArrowAltCircleUp, FaRegShareSquare} from "react-icons/fa";
+import { AiOutlineDollar } from "react-icons/ai";
+
 
 const Post = ({photo,title, description}) => {
   return (
     <div className="card mb-3 mt-2" style={{width: "674px", height :"max-content"}}>
   <div className="row g-0">
-    <div className="col-md-2" >
-
-      <img src={photo} className="img-fluid" alt="..." 
-      style={{backgroundRepeat: "no-repeat",backgroundPosition: "50%",
-      borderRadius: "50%",
-      backgroundSize: "100% auto", 
-      height : "100px", 
-      margin:"10px"}}/>
-
+    <div className="col-md-2 col-rows-2" >
+      <div className='row'>
+        <img src={photo} className="img-fluid" alt="..." 
+        style={{backgroundRepeat: "no-repeat",backgroundPosition: "50%",
+        borderRadius: "50%",
+        backgroundSize: "100% auto", 
+        height : "100px", 
+        margin:"10px"}}/>
+      </div>
+      <div className='row row-cols-3'>
+        <div className='col'> <FaArrowAltCircleUp /> </div>
+        <div className='col'> <AiOutlineDollar /> </div>
+        <div className='col'> <FaRegShareSquare /> </div>
+      </div>
     </div>
     <div className="col-md-8">
       <div className="card-body">
