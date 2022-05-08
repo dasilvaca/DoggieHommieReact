10066f2e2b6e47829148050532e7d176ceec94f8
dasilvaca <dasilvaca@unal.edu.co>
2022-05-07 20:11:26 -0500

    Now createPost redirects to new-post

--- src/components/createPost.js
+++ src/components/createPost.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProfilePic from '../assets/img/4.png'
 
 const CreatePost = () => {
+
     return (
         <>
             <div className="container justify-content-center">
                 <div className="row justify-content-center">
                     <div className="card mb-3 mt-2" style={{ width: "674px", height: "max-content" }}>
-                        <div className="row g-0" style={{marginTop:"10px"}}>
+                        <div className="row g-0" style={{ marginTop: "10px" }}>
                             <div className="col-1">
                                 <img src={ProfilePic} className="createPostProfilePic" />
                             </div>
                             <div className="col-11">
-                                <input style={{}} type="text" className="form-control" placeholder="What's on your mind?" />
+                                <Link to="/new-post" style={{textDecoration: "None"}}>
+                                    <input style={{}} type="text" className="form-control" placeholder="Ask for help" onClick={{}} />
+                                </Link>
                             </div>
+                        </div>
+                        <div className="row g-0" /*style={{ marginTop: "10px" }}*/>
                         </div>
                     </div>
                 </div>
