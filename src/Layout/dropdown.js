457cb3157fe402339133f99e5f562ec84afcaebe
Juan Pablo Garson Parra <juromerop@unal.edu.co>
2022-05-23 01:03:18 -0500

    Todo pasado a dropdown.js

--- src/Layout/dropdown.js
+++ src/Layout/dropdown.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Avatar from "../assets/img/Avatar.jpg";
+import { Link } from "react-router-dom";
 
 const Dropdown = () => {
+    const signOut = () => {
+        localStorage.removeItem("username");
+        localStorage.removeItem("token");
+        localStorage.removeItem("user_id");
+        localStorage.removeItem("user");
+        window.location.href = "/";
+      }
   return (
-    <div>dropdown</div>
+    <>
+        <Link to="/profile">
+        <img src={Avatar} style={{ height: "2rem", borderRadius: "30px", alt: "Avatar" }} />
+        </Link>
+        <Link to="/" onClick={signOut}>Sign Out</Link>
+    </>
   )
 }
 
--- src/Layout/navbar.js
+++ src/Layout/navbar.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/img/Doggie Hommie.png";
-import Avatar from "../assets/img/Avatar.jpg";
+import Dropdown from './dropdown.js';
 import logged from '../global variables'
 import backend_host from '../global variables'
 import { useContext } from "react";
 
 
 const Navbar = () => {
-  const signOut = () => {
-    localStorage.removeItem("username");
-    localStorage.removeItem("token");
-    localStorage.removeItem("user_id");
-    localStorage.removeItem("user");
-    window.location.href = "/";
-  }
   return (
     <div className="navbar">
       <div id="logo">
@@ -25,12 +18,7 @@ const Navbar = () => {
       <div id="nav-links">
         {
           localStorage.getItem('username')? (
-            <>
-              <Link to="/profile">
-                <img src={Avatar} style={{ height: "2rem", borderRadius: "30px", alt: "Avatar" }} />
-              </Link>
-              <Link to="/" onClick={signOut}>Sign Out</Link>
-            </>
+              <Dropdown/ >
 
           ) : (
             <>
