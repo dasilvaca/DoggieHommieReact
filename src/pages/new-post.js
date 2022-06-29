3f900db9423a0b545784446152855d78e20413fb
lauraaac <lceballosa@unal.edu.co>
2022-06-28 20:24:43 -0500

    fix back deploy

--- src/pages/new-post.js
+++ src/pages/new-post.js
@@ -48,9 +48,9 @@ const NewPost = () => {
     const handleSubmit = async (event) => {
         event.preventDefault()
         // var x = await axios.post('http://localhost:8000/post/', post_req)//, fetch)
-        // var x = await axios.post('https://backdoggiehommie.herokuapp.com/post/', post_req)//, fetch)
+        var x = await axios.post('https://backdoggiehommie.herokuapp.com/post/', post_req)//, fetch)
         
-        var x = await axios.post(post_url, post_req)//, fetch)
+        //var x = await axios.post(`${post_url}`, post_req)//, fetch)
 
 
         if (x.status === 200) {
